9c7656b3ce384f10321ede6aec8db824eb061ae6
pimowo <124725348+pimowo@users.noreply.github.com>
2025-01-21 00:46:56 +0100

    przenosiny

---
+++ include/config.h
@@ -0,0 +1,13 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Podstawowa konfiguracja pinów
+#define MOTOR_PIN 26
+#define DISPLAY_PIN 21
+#define SENSOR_PIN 22
+
+// Stałe konfiguracyjne
+#define MAX_SPEED 25  // km/h
+#define MAX_POWER 250 // W
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+
+void setup() {
+    Serial.begin(115200);
+    Serial.println("E-Bike System Starting...");
+}
+
+void loop() {
+    // Główna pętla programu
+    delay(1000);
+}
